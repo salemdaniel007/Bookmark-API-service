f9fed7916862ef9e080164def0fe0359aa730460
salemdaniel007 <salemdaniel007@gmail.com>
2023-01-16 02:24:23 +0100

    bootstrap

---
+++ src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from 'src/auth/auth.module';
+import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+
+@Module({
+  imports: [AuthModule, UserModule, BookmarkModule],
+})
+export class AppModule {}
