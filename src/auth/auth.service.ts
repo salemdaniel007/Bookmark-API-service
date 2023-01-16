c9af9b113f9528ed21f00227435c1ba152801c15
salemdaniel007 <salemdaniel007@gmail.com>
2023-01-16 09:58:44 +0100

    set up basic auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,54 @@
-import { Injectable } from "@nestjs/common";
-import { PrismaService } from "src/prisma/prisma.service";
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { domainToASCII } from 'url';
+import { debugPort } from 'process';
+import { UnsubscriptionError } from 'rxjs';
 
-@Injectable ()
+@Injectable()
 export class AuthService {
-    constructor(private prisma: PrismaService) {}
-    signup () {
-        return {
-            msg: 'I have signed up'
-        }
-    }
-    signin () {
-        return {
-            msg: 'I have signed in'
-        }
-    }
+  constructor(private prisma: PrismaService) {}
+  async signup(dto: AuthDto) {
+    try {
+      // generate the password hash
+      const hash = await argon.hash(dto.password);
 
+      // save the new user in the db
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+      delete user.hash;
+      // return the saved user
+      return user;
+    } catch (error) {
+      if (error.code === 'P2002') {
+        throw new ForbiddenException('Credentials taken');
+      }
+      throw error;
+    }
+  }
+  async signin(dto: AuthDto) {
+    // find user by email
+
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    // if user does not exist, throw exception
+    if (!user) throw new ForbiddenException('Credentials Incorrect');
+    // compare password
+
+    const pwMatches = await argon.verify(user.hash, dto.password);
+    // if password incorrect throw exception
+    if (!pwMatches) throw new ForbiddenException('Credentials Incorrect');
+    delete user.hash;
+    // send back user
+
+    return user;
+  }
 }
